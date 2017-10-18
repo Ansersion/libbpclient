babbf0ce3444659dac012c8732b84de5cddc3098
Ansersion <ansersion@gmail.com>
2017-10-18 08:09:36 -0700

    Modify send&recv logic for example

--- example/bpclient.c
+++ example/bpclient.c
@@ -23,6 +23,7 @@ int main()
 	int n;
 	int len;
 	int i;
+	int multiplier;
 
 	BP_UINT8 * user_name = "Ansersion";
 	BP_UINT8 * password = "a123456";
@@ -56,7 +57,7 @@ int main()
 	*pbuf++ = BP_LEVEL;
 	rmn_len++;
 
-	*pbuf++ = BP_VRB_FLAG_USER_NAME_MSK | BP_VRB_FLAG_PASSWORD_MSK | BP_VRB_FLAG_DEV_LOGIN_MSK;
+	*pbuf++ = BP_VRB_FLAG_USER_NAME_MSK | BP_VRB_FLAG_PASSWORD_MSK | BP_VRB_FLAG_DEV_CLNT_MSK;
 	rmn_len++;
 
 	pbuf = BP_SetBig16(pbuf, client_id);
@@ -95,20 +96,75 @@ int main()
 	printf("\n");
 
 	n=send(conndfd,buf,rmn_len + 2,0);
+	if(n != rmn_len + 2) {
+		close(conndfd);
+		perror("Send error");
+		return -1;
+	}
 
-	n=recv(conndfd,buf,2048, 0);
+	n=recv(conndfd,buf,3, MSG_WAITALL);
+	if(3 != n) {
+		close(conndfd);
+		perror("Recv error 1");
+		return -2;
+	}
+	multiplier = 1;
+	i = 0;
+	len = 0;
+	do {
+		i++;
+		len += (buf[i] & 0x7F) * multiplier;
+		multiplier *= 128;
+		if(multiplier > 128) {
+			close(conndfd);
+			printf("parse remaining length error");
+			return -3;
+		}
+	} while((buf[i] & 0x80) != 0);
+
+	if(len < 128) len--;
+
+	n += recv(conndfd,buf+3,len, MSG_WAITALL);
+
+	if(n != len + 3) {
+		close(conndfd);
+		printf("Recv error 2");
+		return -2;
+	}
 	printf("recv %d bytes\n", n);
 	for(i = 0; i < n; i++) {
 		printf("%02x ", buf[i]);
 	}
 	printf("\n");
+	BP_GetBig16(buf + 6, &client_id);
+	printf("client id = %d\n", client_id);
 
-	while(1) {
-		sleep(1);
+	pbuf = buf;
+	pbuf = BP_make_fix_head(pbuf, BP_PACK_TYPE_DISCONN_MSK, 0);
+	rmn_len = 0;
+
+	pbuf = BP_SetBig16(pbuf, client_id);
+	rmn_len += 2;
+
+	pbuf = buf;
+	rmn_len += 4;
+	BP_make_fix_head(pbuf, BP_PACK_TYPE_DISCONN_MSK, rmn_len);
+	pbuf = buf;
+	crc = BP_calc_crc32(pbuf, rmn_len-4+2);
+	BP_SetBig32((buf+rmn_len+2-4), crc);
+
+	n=send(conndfd,buf,rmn_len + 2,0);
+	if(n != rmn_len + 2) {
+		close(conndfd);
+		perror("Send error");
+		return -3;
 	}
 
-	// printf("Recv %d byte from Server\n",n);
-	// printf("%s\n",buf);
+	for(i = 0; i < rmn_len + 2; i++) {
+		printf("%02x ", buf[i]);
+	}
+	printf("\n");
+
 	close(conndfd);
 	return 0;
 }
